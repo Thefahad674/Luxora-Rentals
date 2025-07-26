019de3db9ab491578b14ebe49aa2e7ef78d10687
Thefahad674 <fahadf30511@gmail.com>
2025-07-26 12:51:33 +0530

    dashboard

--- src/components/owner/SideBar.jsx
+++ src/components/owner/SideBar.jsx
@@ -19,7 +19,7 @@ const SideBar = () => {
         
         <div className='group relative'>
            <label htmlFor="image">
-            <img src={image ? URL.createObjectURL(image) : user?.image } alt="" />
+            <img src={image ? URL.createObjectURL(image) : user?.image } alt="" className='h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto' />
            <input type="file" id='image' accept='image/*' hidden onChange={(e)=>setImage(e.target.files[0])} />
 
            <div className='absolute hidden top-0 right-0 left-0 bottom-0 bg-black/10 rounded-full group-hover:flex items-center justify-center cursor-pointer'>
